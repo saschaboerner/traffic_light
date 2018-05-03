54609527e5d8ace7e8ad81ff5505bd8cbceabf97
Peter Turczak <peter@turczak.de>
2018-05-03 20:06:55 +0000

    Preliminary visualization

--- website/js/ampel.js
+++ website/js/ampel.js
@@ -21,7 +21,21 @@ traffic_light = function (subdir, root_element) {
 		},
 	this.got_answer=function(data){
 		$(".battvoltage", this.root_element).val(data.batt_voltage/100.0);
+		if (data.lamp_currents[0]>10) this.red_circle.show(); else this.red_circle.hide();
+		if (data.lamp_currents[1]>10) this.yellow_circle.show(); else this.yellow_circle.hide();
+		if (data.lamp_currents[2]>10) this.green_circle.show(); else this.green_circle.hide();
+
 	};
+	$.ajax("/image/ampel.svg",
+		{
+		dataType:"text",
+		success:function(data){
+			$(".tlpicture", this.root_element).replaceWith(data);
+			me.red_circle = $(".red", this.root_element);
+			me.yellow_circle = $(".yellow", this.root_element);
+			me.green_circle = $(".green", this.root_element);
+			}
+		})
 	// Now finally run the stuff
 	me.run(250);
 	$(".setred",this.root_element).click( function(){
