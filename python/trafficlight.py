70e3c6c727665df9d712de818defa782b9ba6311
Peter Turczak <peter@turczak.de>
2018-05-04 17:41:34 +0200

    Added remote traffic light code and isGood in json

--- python/trafficlight.py
+++ python/trafficlight.py
@@ -1,22 +1,30 @@
 import json
 import logging
 from twisted.internet.serialport import SerialPort
-from twisted.internet import reactor
+from twisted.internet import reactor, task
+from twisted.web.client import Agent, readBody
 from twisted.protocols import basic
+from twisted.python import log
+from time import time
 
 class TrafficLight(object):
+    maxage = 1
 
     def __init__(self):
         self.state = 99
         self.batt_voltage = 0
         self.lamp_currents = [0]*3
-        self.seen = False
+        self.last_seen = 0
+
+    def seen(self):
+        return (time()-self.last_seen) < self.maxage
 
     def to_json(self):
         data = {
             "state":self.state,
             "batt_voltage":self.batt_voltage,
-            "lamp_currents":self.lamp_currents
+            "lamp_currents":self.lamp_currents,
+            "good":self.isGood()
             }
         return json.dumps(data)
     
@@ -33,7 +41,7 @@ class TrafficLight(object):
         self.sendUpdate()
 
     def isGood(self):
-        return self.state != 9
+        return (self.state != 9) and (self.seen())
 
     def __str__(self):
         return "TrafficLight(state={}, batt_voltage={}, lamp_currents={})".format(self.state, self.batt_voltage, self.lamp_currents)
@@ -55,6 +63,44 @@ class TrafficLightGroup(object):
             return None
         return True
 
+class TrafficLightRemote(TrafficLight):
+    def __init__(self, url, interval):
+        TrafficLight.__init__(self)
+        self.poll_loop = task.LoopingCall(self.poll_remote)
+        self.agent = Agent(reactor)
+        self.remote_url = url
+        self.running_request = None
+        self.error_count = 0
+        self.poll_loop.start(interval)
+        
+    def poll_remote(self):
+        '''
+        Polls remote host to get its state
+        '''
+        if self.running_request is not None:
+            self.error_count += 1
+            self.running_request.cancel()
+
+        try:
+            self.running_request = self.agent.request(b"GET", self.remote_url)
+            self.running_request.addCallback(self.request_handler)
+            self.running_request.addErrback(log.err)
+        except Exception as e:
+            print ">>>>{}".format(e)
+    
+    def request_handler(self, response):
+        d = readBody(response)
+        # TODO maybe move it until validation?
+        self.error_count = 0
+        d.addCallback(self.on_data_received)
+        d.addErrback(log.err)
+        self.running_request = None
+        return d
+
+    def on_data_received(self, body):
+        self.from_json(body)
+        self.last_seen = time()
+
 class TrafficLightSerial(basic.LineReceiver, TrafficLight):
 
     delimiter = '\n'.encode('ascii')
@@ -65,14 +111,27 @@ class TrafficLightSerial(basic.LineReceiver, TrafficLight):
                    }
 
     @classmethod
-    def open(cls, port, reactor=reactor):
+    def open(cls, port, reset_pin=None, reactor=reactor):
         local_light = cls()
         serial = SerialPort(baudrate=19200, deviceNameOrPortNumber=port, protocol=local_light, reactor=reactor)
         local_light.setSerial(serial)
+        local_light.setReset(reset_pin)
         return local_light
 
     def setSerial(self, serial):
         self.serial = serial
+
+    def setReset(self, pin):
+        if pin is None:
+            self.reset_name = None
+            return
+
+        self.reset_name = "/sys/class/gpio/gpio{}/value".format(pin)
+        try:
+            with open("/sys/class/gpio/export", "w") as f:
+                f.write("{}\n".format(pin))
+        except Exception as e:
+            logging.error("Could not open/write exports in gpiofs: {}".format(e))
 
     def lineReceived(self, line):
         # Ignore blank lines
@@ -80,6 +139,7 @@ class TrafficLightSerial(basic.LineReceiver, TrafficLight):
         line = line.decode("ascii").strip()
         try:
             (self.state, self.batt_voltage, self.error_state, self.lamp_currents[0], self.lamp_currents[1], self.lamp_currents[2]) = line.split(" ")
+            self.last_seen = time()
         except ValueError:
             logging.info("Received garbled line")
         #logging.warning("update myself: {}".format(self))
