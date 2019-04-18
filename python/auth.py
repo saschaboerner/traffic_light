1d2c6f7b23e1771b868d2536662030cbd8666484
Peter Turczak <peter@turczak.de>
2019-04-18 16:07:58 +0200

    Linted

--- python/auth.py
+++ python/auth.py
@@ -8,13 +8,12 @@ from hmac import HMAC
 from twisted.web import resource
 
 
-
 class TransportWrapper(object):
 
     def __init__(self, key):
         self.key = key
         self.digestmod = hashlib.sha256
-        self.charset = [x for x in  string.printable.strip()]
+        self.charset = [x for x in string.printable.strip()]
 
     def makeChallenge(self):
         return "".join(random.sample(self.charset, 32))
@@ -26,25 +25,27 @@ class TransportWrapper(object):
         clone['challenge'] = challenge
         raw = json.dumps(clone)
         hashsum = HMAC(self.key, msg=raw, digestmod=self.digestmod).hexdigest()
-        
-        return json.dumps({"raw":raw, "hash":hashsum})
+
+        return json.dumps({"raw": raw, "hash": hashsum})
 
     def decapsulate(self, message, challenge):
-        #assert(type(message) is str)
+        # assert(type(message) is str)
         packet = json.loads(message)
         if 'raw' not in packet or 'hash' not in packet:
             raise AttributeError("Packet malformatted")
 
-        hashsum = HMAC(self.key, msg=packet['raw'], digestmod=self.digestmod).hexdigest()
+        hashsum = HMAC(self.key, msg=packet['raw'],
+                       digestmod=self.digestmod).hexdigest()
 
         if not (hashsum == packet['hash']):
             return False
 
         raw_pkt = json.loads(packet['raw'])
         if raw_pkt['challenge'] != challenge:
-            raise ValueError("Challenge mismatch")        
+            raise ValueError("Challenge mismatch")
 
         return raw_pkt
+
 
 class Authenticator(resource.Resource, TransportWrapper):
     isLeaf = True
